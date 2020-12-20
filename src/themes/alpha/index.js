1e0369fb7655fcf2cedcb2827cf530a68c5d96a8
Bruno Rodrigues <brunowebr@gmail.com>
2020-12-20 00:37:31 -0300

    Add contactForm component

--- src/themes/alpha/index.js
+++ src/themes/alpha/index.js
@@ -5,6 +5,7 @@ import GoogleFontLoader from 'react-google-font-loader';
 // Components
 import Header from './components/header';
 import ListingItems from './components/listingItems';
+import ContactForm from './components/contactForm';
 
 // Services
 import Mock from '../services/api/mock';
@@ -20,14 +21,16 @@ function Home() {
             {/* Load Google Font */}
             <GoogleFontLoader fonts={[{ font: 'Montserrat', weights: [500, 600] }]} />
             
-            {/* Header */}
-            <Header 
-                name={Mock.user.fullname}
-                website={Mock.user.website}
-            />
-            <div className="container">
-                <ListingItems />
-            </div>
+            <main>
+                {/* Header */}
+                <Header 
+                    name={Mock.user.fullname}
+                    website={Mock.user.website}
+                />
+                <div className="container">
+                    <ListingItems />
+                </div>
+            </main>
         </>
     )
 }
