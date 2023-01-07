d506cf0d7cab54b4a29902f7a07056cf58096593
Bruno Rodrigues <brunowebr@gmail.com>
2023-01-07 11:37:26 -0300

    Add base nextjs structure with typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+export default function Home() {
+  return (
+    <div className={inter.className}>Hello Linksforall</div>
+  )
+}
