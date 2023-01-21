502ace55252a77f9fe68643ba92ae9e8cf8c742a
Bruno Rodrigues <brunowebr@gmail.com>
2023-01-21 17:40:46 -0300

    Add GA4 support (#5)
    * Add GA4 support
    
    * Add some meta tags
    
    * Upgrade version to 3.0.1
    
    * Add utils path name to tsconfig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,12 @@ import Head from 'next/head';
 
 import { Alpha } from '@themes/alpha';
 import { DATA } from '@lib/data';
+import { GA4 } from '@utils/ga4';
 
 export default function Home() {
   const { user } = DATA;
+  const GA4ID = process.env.NEXT_PUBLIC_GA4_ID;
+
   return (
     <>
       <Head>
@@ -12,8 +15,15 @@ export default function Home() {
         <meta name="description" content={user.bio} />
         <link rel="icon" href="/images/favicon.png" />
         <link rel="icon" href="/images/favicon.svg" type="image/svg+xml" />
+
+        <meta name="robots" content="index,follow" />
+
+        <meta property="og:title" content={user.fullname} />
+        <meta property="og:description" content={user.bio} />
+        <meta property="og:type" content="website" />
       </Head>
       <Alpha data={DATA} />
+      {GA4ID && <GA4 ga4Id={GA4ID} />}
     </>
   );
 }
