def8f991ee1897630974428e3a231a14b0962864
Bruno Rodrigues <brunowebr@gmail.com>
2024-02-13 18:17:40 -0300

    Upgrade NextJS version (#8)
    * chore: upgrade nextjs version to latest version (14.1.0)
    
    * chore: uninstall @next/font to use next/font
    
    * Update image domains to use new feature from Next with remotePatterns

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['avatars3.githubusercontent.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars3.githubusercontent.com'
+      }
+    ]
   }
 };
 
